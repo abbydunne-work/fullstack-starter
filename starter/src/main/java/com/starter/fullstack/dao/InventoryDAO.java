402d43eb32f00862df7e2e1896332f35da4ffab2
Abigail Dunne <abby.dunne@interclypse.com>
2025-10-23 08:02:44 -0400

    StarterApp Task1: initial progress of adding save inventory method asnd test

--- starter/src/main/java/com/starter/fullstack/dao/InventoryDAO.java
+++ starter/src/main/java/com/starter/fullstack/dao/InventoryDAO.java
@@ -51,9 +51,20 @@ public class InventoryDAO {
    * @return Created/Updated Inventory.
    */
   public Inventory create(Inventory inventory) {
-    // TODO
-    return null;
+    inventory.setId(null);
+    return this.mongoTemplate.save(inventory);
   }
+
+//    /**
+//     * Save Inventory.
+//     * @param inventory Inventory to Save/Update.
+//     * @return Created/Updated Inventory.
+//     */
+//    public Inventory create(Inventory inventory) {
+//
+//        inventory.setId(null);
+//        return this.mongoTemplate.save(inventory);
+//    }
 
   /**
    * Retrieve Inventory.
--- starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
+++ starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
@@ -52,4 +52,24 @@ public class InventoryDAOTest {
     List<Inventory> actualInventory = this.inventoryDAO.findAll();
     Assert.assertFalse(actualInventory.isEmpty());
   }
+
+    /**
+     * Test Create method.
+     */
+//    @Test
+//    public void create() {
+//      Inventory inventoryOne = new Inventory();
+//      inventoryOne.setName(NAME);
+//      inventoryOne.setProductType(PRODUCT_TYPE);
+//      Inventory inventoryTwo = new Inventory();
+//      inventoryTwo.setName(NAME);
+//      inventoryTwo.setProductType(PRODUCT_TYPE);
+//      this.inventoryDAO.create(inventoryOne);
+//      this.inventoryDAO.create(inventoryTwo);
+//      List<Inventory> actualInventory = this.inventoryDAO.findAll();
+//      // Asserting equal to one because in this case it would increment the amount
+//      Assert.assertEquals(1, actualInventory.size());
+//      var firstItem = actualInventory.get(0);
+//      Assert.assertEquals(2.0, firstItem.getAmount());
+//    }
 }
